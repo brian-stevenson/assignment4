abb7be0354f1484e4a1cdeab65ea5200b72bd7b1
brian-stevenson <brs128@pitt.edu>
2016-11-28 09:02:24 -0500

    Brian Stevenson- Assignment Complettion Update
    Created an autocomplete feature as required.

--- assignment4.js
+++ assignment4.js
@@ -1,3 +1,4 @@
+
 // The anonymous function below will fire on page load
 
 // Some things to consider
@@ -11,7 +12,63 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
-})();
+var clicked = 0;
+var result=[];
+var i =0;
+var j;
+var tracker = "";
+var charr;
+var first =1;
+var wasBackspace = 1;
+var checker="";
+var once = 0;
+
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://www.mattbowytz.com/simple_api.json?data=programming ',
+        async:true,
+        dataType: "json",
+        //data: {get_param: 'all'},
+        success: function(info)
+        {
+            result = info.data;
+        }
+    });
+});
+
+
+$('html').keyup(function(e){
+
+    if(e.keyCode == 8) {
+        tracker = tracker.substr(0, tracker.length - 1);
+        //console.log(tracker);
+    }
+
+    if(e.keyCode != 16) {
+        $('.results').html("");
+        for (var k = 0; k < result.length; k++) {
+            for (j = 0; j < result[k].length; j++) {
+                checker += (result[k])[j];
+                if ((checker == tracker)) {
+
+                        //$('footer').append("Are you looking for: " + result[k] + "?<br/>");
+                    $('.results').append(result[k]+"<br/>");
+
+
+
+                }
+            }
+            checker = "";
+        }
+    }
+})
+
+
+$(document).keypress(function(e) {
+
+
+        //console.log(String.fromCharCode(e.which));
+        tracker += String.fromCharCode(e.which);
+        //console.log(tracker);
+});
